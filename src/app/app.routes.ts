57994c788ecc4d19bcfcf9d6a26646f2424af1da
Apostolos Medi Gjoni <122259217+apostolosgj@users.noreply.github.com>
2025-01-17 07:45:06 +0000

    succes stories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AiTextGenComponent } from './ai-text-gen/ai-text-gen.component';
 import { QuizComponent } from './quiz/quiz.component';
+import { SuccesFactorsComponent } from './success-factors/success-factors.component';
 
 export const routes: Routes = [
   { path: 'ki-text', component: AiTextGenComponent },
   { path: 'quiz', component: QuizComponent },
+  { path: 'success', component: SuccesFactorsComponent },
 ];
