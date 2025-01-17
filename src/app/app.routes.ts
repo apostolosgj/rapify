b8176243f8c90437339c850c95e90d20fbe7f4b9
Apostolos Medi Gjoni <122259217+apostolosgj@users.noreply.github.com>
2025-01-17 07:36:56 +0000

    quiz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AiTextGenComponent } from './ai-text-gen/ai-text-gen.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 export const routes: Routes = [
-    { path: "ki-text", component: AiTextGenComponent}
+  { path: 'ki-text', component: AiTextGenComponent },
+  { path: 'quiz', component: QuizComponent },
 ];
